8e19730ffc35ea8532a32f01561adb825648fd0b
Captain Nick Lucifer* <naryal2580@gmail.com>
2022-03-02 06:32:56 +0545

    misc firefox fix.

--- popup/main.js
+++ popup/main.js
@@ -1,21 +1,33 @@
 'use strict';
 
+function close_current_tab() {
+  chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+    chrome.tabs.remove(tabs[0].id);
+  })
+}
+
 function main() {
-	let msg = document.querySelector('#msg');
-	document.addEventListener('keydown', (event) => {
-		if ( event.key.toLowerCase() === 'y' ) {
-			chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
-				chrome.tabs.remove(tabs[0].id);
-			}); window.close();
-		} else if ( event.key.toLowerCase() === 'n' ) {
-			window.close();
-		} else {
-			// TODO: Animate YES/NO Buttons.
-		}
-	});
-	// TODO: Animate YES/NO Buttons.
-	msg.innerText += 'Are you sure? You want to close this tab.\n\n';
-	msg.innerHTML += '<span style="font-weight: bold;">[Y]35</span>\t<span style="color: red;">[N]0</span>';
+  let msg = document.querySelector('#msg');
+  document.addEventListener('keydown', (event) => {
+    if ( event.key.toLowerCase() === 'y' ) {
+      close_current_tab();
+    } else if ( event.key.toLowerCase() === 'n' ) {
+      window.close();
+    } else {
+      // TODO: Animate YES/NO Buttons.
+    }
+  });
+  // TODO: Animate YES/NO Buttons.
+  msg.innerText += 'Are you sure? You want to close this tab.\n\n';
+  msg.innerHTML += '<span style="font-weight: bold;" id="y">[Y]35</span>\t<span style="color: red;" id="n">[N]0</span>';
+  let ids = ['y', 'n'];
+  for ( let i=0; i < ids.length; i++ ) {
+    document.getElementById(ids[i]).addEventListener('click', function() {
+      if ( ! Boolean(Number(i)) ) {
+        close_current_tab();
+      }; window.close();
+    })
+  }
 }
 
 document.addEventListener('DOMContentLoaded', main);
